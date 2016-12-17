0bca1395aecddd67b604021ca43cbbc5716307bf
konidk <nicolas.cortella@gmail.com>
2016-12-17 02:14:46 +0100

    NEW DESIGN: Addrecipe

--- FitFood_APP/app/src/main/java/konid/soxzz5/fitfood/AddRecipe.java
+++ FitFood_APP/app/src/main/java/konid/soxzz5/fitfood/AddRecipe.java
@@ -8,6 +8,7 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
+import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
