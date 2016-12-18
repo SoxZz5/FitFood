d03137e7422124f9ca24209fef05cd9b2cc6ad43
konidk <nicolas.cortella@gmail.com>
2016-12-18 18:33:35 +0100

    Single display  recipe v2

--- FitFood_APP/app/src/main/java/konid/soxzz5/fitfood/fitfood_fragment/SingleRecipeDisplay.java
+++ FitFood_APP/app/src/main/java/konid/soxzz5/fitfood/fitfood_fragment/SingleRecipeDisplay.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.TextView;
 
 import konid.soxzz5.fitfood.R;
 
@@ -13,10 +15,22 @@ import konid.soxzz5.fitfood.R;
  */
 
 public class SingleRecipeDisplay extends Fragment {
+
+    private GridView ingredientsGRID;
+    private GridView stepsGRID;
+    private TextView recipeTitle;
+    private TextView recipeAuthor;
+    private int recipeId = 0;
+
     @Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         //DO SOMETHING WITH
         View v = inflater.inflate(R.layout.recipe_display_full_info, container, false);
+        ingredientsGRID = (GridView) v.findViewById(R.id.ingredients_grid);
+        stepsGRID = (GridView) v.findViewById(R.id.steps_grid);
+        recipeTitle = (TextView) v.findViewById(R.id.recip_title);
+        recipeAuthor = (TextView) v.findViewById(R.id.recipe_author);
+
 
         return v;
     }
